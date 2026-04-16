760d3193ebdf21a41509846861140b64ea4870a4
sahaana150@gmail.com <sahaana150@gmail.com>
2026-04-16 11:08:28 +0530

    UC4: Display Hello for multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
+
     }
-}    
+}
 
 
